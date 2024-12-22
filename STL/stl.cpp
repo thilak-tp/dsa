dad0811eb57c4aca434433059a0683279527bc70
Thilak T P Linux <tpthilaktp@gmail.com>
2024-12-22 18:33:28 +0000

    Added some pattern code

---
+++ STL/stl.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<ulitity>
+
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+
+int main()
+{
+	Pair<int><int> pair = {1,2};
+	cout<<"The first element of the pair is "<<pair.first<<" \nThe second element of the pair is "<<pair.second
+
+	return 0;
+}
--- simple/patterns/patterns.cpp
+++ simple/patterns/patterns.cpp
@@ -3,7 +3,221 @@
 using std::cout;
 using std::cin;
 using std::endl;
+/*
+the below function is for a pattern like below
+*****
+*   *
+*   *
+*   *
+*****
+*/
 
+void squarePattern1(int n)
+{
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0; j<n; j++)
+		{
+			if(i==0 | j==0 | i == n -1 | j == n-1)
+			{
+				cout<<"*";
+			}
+			else
+			{
+				cout<<" ";
+			}
+		}
+		cout<<endl;
+	}
+}
+
+/*
+the below function is for a pattern like below
+A
+AB
+ABC
+ABCD
+ABCDE
+*/
+
+void alphaPattern1(int n)
+{
+	for(int i=9; i<n; i++)
+	{	
+		for(char ch = 'A'; ch <= 'A' + i; ch++)
+		{
+			cout<<ch<<" ";
+		}
+  		cout<<endl;
+	}
+}
+
+/*
+the below function is for a pattern like below
+1
+0 1
+1 0 1 
+0 1 0 1
+*/
+
+void numContinueTriBinary(int n)
+{
+	int number = 1;
+	int i;
+	for(i=0; i<n;i++)
+	{
+
+		if(i%2 == 0)
+			 number = 1;
+		else
+			
+			 number = 0;
+	
+		for(int j=0;j<=i; j++)
+		{
+			cout<<number;
+			number =1- number;			
+
+		}
+		cout<<endl;
+	}
+}
+
+
+/*
+the below function is for a pattern like below
+1
+2 3
+4 5 6
+7 8 9 10
+*/
+
+void numContinueTri(int n)
+{
+	int number =1;
+	for(int i=0; i<n;i++)
+	{
+		for(int j=0;j<=i; j++)
+		{
+			cout<<number++;
+
+		}
+		cout<<endl;
+	}
+}
+
+
+
+
+
+/*
+the below function is for a pattern like below
+*****
+ ***
+  *
+*/
+
+void equiTriangleReversed(int n)
+{
+	for( int i=0; i<n; i++)
+	{
+
+		for( int j=0; j<i; j++)
+		{
+			cout<<" ";
+		}
+		
+		for( int j=0; j<(2*n)-(2*i+1); j++)
+		{
+			cout<<"*";
+		}
+		
+		for( int j=0; j<n; j++)
+		{
+			cout<<" ";
+		}
+			
+		cout<<endl;
+	}
+}
+
+
+/*
+the below function is for a pattern like below
+  *  
+ *** 
+*****
+*/
+
+void equiTriangle(int n)
+{
+	for( int i=0; i<n; i++)
+	{
+
+		for( int j=0;j<n-i-1; j++)
+		{
+			cout<<" ";
+		}
+		
+		for( int j=0;j<2*i+1; j++)
+		{
+			cout<<"*";
+		}
+		
+		for( int j=0;j<n-i-1; j++)
+		{
+			cout<<" ";
+		}
+			
+		cout<<endl;
+	}
+}
+
+
+
+
+/*
+the below function is for a pattern like below
+12345
+1234
+123
+12
+1
+*/
+
+void invertedtrianglenumber(int n)
+{
+	for( int i=0; i<n; i++)
+	{
+		for( int j=n; j>i; j--)
+		{
+			cout<<n-j+1;
+		}
+		cout<<endl;
+	}
+}
+
+/*
+The below function is for a pattern like below
+54321
+5432
+543
+54
+5
+
+*/
+
+void invertedTriangle(int n)
+{
+	for( int i=0; i<n; i++)
+	{
+		for( int j=n; j>i; j--)
+		{
+			cout<<j;
+		}
+		cout<<endl;
+	}
+}
 
 /*
 //The below function is for a pattern like below
@@ -139,7 +353,7 @@ int main()
 	cout<<"Enter the N values:"<<endl;
 	cin>>n;
 	//SquarePattern(n);
-	LeftLeaningRightAngleTriInverterNumber(n);
+	squarePattern1(n);
 	return 0;
 }
 
